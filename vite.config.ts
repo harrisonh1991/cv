9ecb05e13c0a1cc7e716666995109266e868ed50
Harrison Kye <50726256+harrisonh1991@users.noreply.github.com>
2025-12-08 22:42:05 +0800

    更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
-import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,11 +10,9 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
-    tailwindcss(),
   ],
   resolve: {
     alias: {
-      // 這行讓 @ 指向 src 資料夾
       '@': path.resolve(__dirname, './src'),
     },
     extensions: ['.js', '.ts', '.jsx', '.tsx'],
